18392eef3fcb3da1ac63bb8ad918c087e305fb6f
Rasmus Hald Nielsen <Murmeldyret@protonmail.com>
2021-09-15 15:56:49 +0200

    Added some progressbars, and added broken async

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ extern crate image;
 use image::{DynamicImage, GenericImageView};
 use imageproc::edges::canny;
 
+use futures::join;
+
 use std::fs::{self, DirEntry};
-use std::thread;
 
 fn main() {
     let paths = detect_samples().unwrap();
     let images = load_samples(paths);
-    let edge_images = detect_edges(images);
+    let _edge_images = detect_edges(images);
 }
 
 fn detect_samples() -> std::io::Result<Vec<DirEntry>> {
@@ -29,7 +30,8 @@ fn load_samples(paths: Vec<DirEntry>) -> Vec<DynamicImage> {
     println!("Loading samples to memory...");
 
     for i in 0..paths.len() {
-        img.push(image::open(paths[i].path()).unwrap())
+        img.push(image::open(paths[i].path()).unwrap());
+        println!("{}/{} completed...", &i + 1, &paths.len());
     }
 
     println!("Samples has been loaded.");
@@ -38,17 +40,23 @@ fn load_samples(paths: Vec<DirEntry>) -> Vec<DynamicImage> {
     img
 }
 
-fn detect_edges(images: Vec<DynamicImage>) -> image::ImageBuffer<image::Luma<u8>, Vec<u8>> {
+async fn detect_edges(images: Vec<DynamicImage>) {
     println!("Processing edges...");
-    
-    let mut edges;
-    for i in 0..images.len() {
-        
-        thread::spawn(move || {
-            canny(&images[i].to_luma8(), 30.0, 150.0);
-        });    
-    }
-    
+    let mut children = Vec::new();
+    let mut edges = Vec::new();
 
-    edges
+    for i in 0..images.len() {
+        children.push(thread_edges(images[i].to_luma8()));
+        println!("Working on {}/{} images.", &i + 1, images.len());
+    }
+
+    for i in 0..images.len() {
+        edges.push(join!(children[i]));
+    }
+
+    
+}
+
+async fn thread_edges(aimage: image::ImageBuffer<image::Luma<u8>, Vec<u8>>) {
+    canny(&aimage, 30.0, 150.0);
 }
