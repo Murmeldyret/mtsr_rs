42c8c4623c6c82ebc6210fb55a474c9746348e73
Rasmus Hald Nielsen <Murmeldyret@protonmail.com>
2021-09-15 23:02:56 +0200

    Still broken

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,17 @@ extern crate image;
 use image::{DynamicImage, GenericImageView};
 use imageproc::edges::canny;
 
-use futures::join;
+use futures::executor::block_on;
+use futures::{channel::mpsc, join};
 
 use std::fs::{self, DirEntry};
+use std::thread;
 
 fn main() {
-    let paths = detect_samples().unwrap();
-    let images = load_samples(paths);
-    let _edge_images = detect_edges(images);
+    let paths = detect_samples().unwrap();                                  //Returns the path of the samples.
+    let images = load_samples(paths);                                    //Returns loaded images
+    let edge_images = detect_edges(images);     //Returns edge detected images
+    block_on(edge_images);
 }
 
 fn detect_samples() -> std::io::Result<Vec<DirEntry>> {
@@ -25,7 +28,7 @@ fn detect_samples() -> std::io::Result<Vec<DirEntry>> {
 }
 
 fn load_samples(paths: Vec<DirEntry>) -> Vec<DynamicImage> {
-    let mut img = Vec::new();
+    let mut img = Vec::with_capacity(paths.len());
 
     println!("Loading samples to memory...");
 
@@ -40,23 +43,34 @@ fn load_samples(paths: Vec<DirEntry>) -> Vec<DynamicImage> {
     img
 }
 
-async fn detect_edges(images: Vec<DynamicImage>) {
+async fn detect_edges(
+    images: Vec<DynamicImage>,
+) -> (image::ImageBuffer<image::Luma<u8>, Vec<u8>>, image::ImageBuffer<image::Luma<u8>, Vec<u8>>, image::ImageBuffer<image::Luma<u8>, Vec<u8>>) {
+    let edges1 = thread_edges(images[0].to_luma8());
+    let edges2 = thread_edges(images[0].to_luma8());
+    let edges3 = thread_edges(images[0].to_luma8());
+
+    join!(edges1, edges2, edges3)
+
+    /*
     println!("Processing edges...");
-    let mut children = Vec::new();
-    let mut edges = Vec::new();
+    let mut children = Vec::with_capacity(images.len());
+    let mut edges = Vec::with_capacity(images.len());
 
     for i in 0..images.len() {
-        children.push(thread_edges(images[i].to_luma8()));
+        children[i] = async {
+            canny(&aimage, 30.0, 150.0);
+            pool.spawn_
+        }
         println!("Working on {}/{} images.", &i + 1, images.len());
     }
 
-    for i in 0..images.len() {
-        edges.push(join!(children[i]));
-    }
-
-    
+    edges = join!(children[0]);
+    */
 }
 
-async fn thread_edges(aimage: image::ImageBuffer<image::Luma<u8>, Vec<u8>>) {
-    canny(&aimage, 30.0, 150.0);
+async fn thread_edges(
+    aimage: image::ImageBuffer<image::Luma<u8>, Vec<u8>>,
+) -> image::ImageBuffer<image::Luma<u8>, Vec<u8>> {
+    canny(&aimage, 30.0, 150.0)
 }
